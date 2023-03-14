70015294a3431da780ace733162bbadd9b4c6e3f
tiffany.duneau <tiffany@incubiq.com>
2023-03-14 12:59:21 +0000

    update package.lock, refactor jupyter display to handle larger files.

--- pytket/extensions/offline_display/__init__.py
+++ pytket/extensions/offline_display/__init__.py
@@ -15,15 +15,62 @@
 """Display a circuit as html locally."""
 
 import os
+import tempfile
+import time
+from typing import Optional, cast
 from jinja2 import PrefixLoader, FileSystemLoader, ChoiceLoader, Environment
 
 from pytket.circuit import Circuit  # type: ignore
 from pytket.circuit.display import (
+    RenderCircuit,
     IncludeRawExtension,
     CircuitRenderer,
     html_loader,
     js_loader,
 )
+
+
+class OfflineRenderer(CircuitRenderer):
+    """Class to manage circuit rendering within a given jinja2 environment."""
+
+    def render_circuit_as_html(
+        self, circuit: RenderCircuit, jupyter: bool = False
+    ) -> Optional[str]:
+        """
+        Render a circuit as HTML for inline display.
+
+        :param circuit: the circuit to render.
+        :param jupyter: set to true to render generated HTML in cell output.
+        """
+        html = super().render_circuit_as_html(circuit, jupyter=False)
+
+        if jupyter:
+            # If we are in a notebook, we can tell jupyter to display the html.
+            # We don't import at the top in case we are not in a notebook environment.
+            import warnings
+            from IPython.display import (  # type: ignore
+                HTML,
+                display,
+            )  # pylint: disable=C0415
+            # Output is large, so must serve it from a file.
+            fp = tempfile.NamedTemporaryFile(
+                mode="w", suffix=".html", delete=False, dir=os.getcwd()
+            )
+            fp.write(cast(str, html))
+            fp.close()
+            try:
+                with warnings.catch_warnings(record=True):
+                    display(HTML(
+                        f'<iframe src="./{os.path.relpath(fp.name)}" width="100%" height="200px" '
+                        f'style="border: none; outline: none; resize: vertical; overflow: auto">'
+                        f'</iframe>'
+                    ))
+                return None
+            finally:
+                # Wait to make sure the file has time to be loaded first.
+                time.sleep(5)
+                os.remove(fp.name)
+        return html
 
 
 # Set up jinja to access our templates
@@ -46,7 +93,7 @@ env = Environment(
 )
 
 # Expose the rendering methods with the local jinja env.
-circuit_renderer = CircuitRenderer(env)
+circuit_renderer = OfflineRenderer(env)
 
 render_circuit_as_html = circuit_renderer.render_circuit_as_html
 render_circuit_jupyter = circuit_renderer.render_circuit_jupyter
